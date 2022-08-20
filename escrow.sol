ad1bd8528982c070b17bebd81094ec0c3ab4cd08
Nuthan Prasad <nuthan1159@gmail.com>
2022-08-20 18:02:32 +0530

    Add files via upload

---
+++ escrow.sol
@@ -0,0 +1,96 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.16;
+
+contract escrow{
+
+    address payable public escrow_AGENT;
+    address payable public buyer;
+    address payable public seller;
+    uint public buyerprice;
+    uint public askingprice;
+
+    bool public BUYER_in;
+    bool public SELLER_in;
+    bool public seller_delivery_SUCCCESS;
+    bool public buyer_refund_SUCCESS;
+
+    enum State{ before_Initialize, awaiting_payment, awaiting_delivery,compeleted, buyer_withdrawn}
+    State public ESCROW_state;
+
+    modifier onlybuyer(){
+        require(msg.sender == buyer);
+        _;
+    }
+
+    modifier onlyescrow_AGENT(){
+        require(msg.sender == escrow_AGENT);
+        _;
+    }
+
+    constructor(address payable _buyer, address payable _seller){
+        ESCROW_state = State.before_Initialize;
+        escrow_AGENT = payable(msg.sender);
+        askingprice = 0.01 ether;
+        buyer = _buyer;
+        seller = _seller;
+
+    }
+
+    //incase buyer sends funds to contract without calling the payment function
+    receive() external payable{
+        buyer_payment();
+    }
+
+    function contract_START() public{
+        require(BUYER_in == false);  
+        if(msg.sender == buyer){
+            BUYER_in = true;
+        }
+        require(SELLER_in == false);
+        if(msg.sender == seller){
+            SELLER_in = true;
+        }
+    }
+
+
+    function escrow_INITIALIZE() public onlyescrow_AGENT {
+        require(BUYER_in == true && SELLER_in == true,"buyer/seller not IN" );
+        require(ESCROW_state == State.before_Initialize,"cant initiate now");
+        ESCROW_state = State.awaiting_payment;
+
+    } 
+
+    function buyer_payment() payable public  onlybuyer {
+        require(ESCROW_state == State.awaiting_payment,"cant pay now");
+
+        buyerprice = msg.value;
+        require(buyerprice == askingprice,"price mismatch");
+
+        escrow_AGENT.transfer(buyerprice);
+        ESCROW_state = State.awaiting_delivery;
+        
+    }
+
+    function delivered() payable public  onlyescrow_AGENT{
+        require(ESCROW_state == State.awaiting_delivery,"payment not yet made by buyer");
+
+        seller.transfer(askingprice);
+        ESCROW_state = State.compeleted;
+
+        seller_delivery_SUCCCESS = true;
+
+    }
+
+    function buyer_withdraw() payable public onlybuyer{
+        require(ESCROW_state == State.awaiting_delivery,"not at this stage bruh");
+
+        buyer.transfer(buyerprice);
+
+        ESCROW_state = State.buyer_withdrawn;
+        buyer_refund_SUCCESS = true;
+        
+
+    }
+
+}
